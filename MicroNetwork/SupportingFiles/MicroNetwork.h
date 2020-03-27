50993fb48683affbf97bc22da05559306e09235a
Dmitrii Celpan <celpand@gmail.com>
2020-03-27 12:23:42 +0200

    Create Xcode project
    The project has universal deployment target with iOS 10
    minimum version requirement. That could be adjusted if
    the necessity will appear.

---
+++ MicroNetwork/SupportingFiles/MicroNetwork.h
@@ -0,0 +1,19 @@
+//
+//  MicroNetwork.h
+//  MicroNetwork
+//
+//  Created by Dmitrii Celpan on 3/27/20.
+//  Copyright © 2020 Dmitrii Celpan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MicroNetwork.
+FOUNDATION_EXPORT double MicroNetworkVersionNumber;
+
+//! Project version string for MicroNetwork.
+FOUNDATION_EXPORT const unsigned char MicroNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MicroNetwork/PublicHeader.h>
+
+
---
+++ MicroNetworkTests/Sources/MicroNetworkTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import MicroNetwork
+
+final class MicroNetworkTests: XCTestCase {
+    func test_whenCalled_ShouldPass() {
+        XCTAssertTrue(true)
+    }
+}
